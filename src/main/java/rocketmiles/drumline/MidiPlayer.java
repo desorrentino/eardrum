29cc037b4293d95c77315dad76e93de835c1e8e1
Damon Sorrentino <damon.sorrentino@gmail.com>
2018-11-16 11:07:44 -0600

    added midi file player

--- src/main/java/rocketmiles/drumline/MidiPlayer.java
+++ src/main/java/rocketmiles/drumline/MidiPlayer.java
@@ -3,11 +3,13 @@ package rocketmiles.drumline;
 import java.io.File;
 import java.io.IOException;
 
+import javax.sound.midi.Instrument;
 import javax.sound.midi.InvalidMidiDataException;
 import javax.sound.midi.MidiSystem;
 import javax.sound.midi.MidiUnavailableException;
 import javax.sound.midi.Sequence;
 import javax.sound.midi.Sequencer;
+import javax.sound.midi.Synthesizer;
 
 public class MidiPlayer {	
 	private Sequence sequence;
@@ -17,7 +19,11 @@ public class MidiPlayer {
 	}
 	
 	public void play(File fileToPlay) {		
-		try {
+		try {			
+			
+			String instrumentName = "Standard Kit";
+			loadInstrument(instrumentName);
+			
 			sequence = MidiSystem.getSequence(fileToPlay);
 			sequencer = MidiSystem.getSequencer(true);
 			sequencer.setSequence(sequence);
@@ -27,18 +33,20 @@ public class MidiPlayer {
 		} catch (InvalidMidiDataException | IOException | MidiUnavailableException e) {
 			// TODO Auto-generated catch block
 			e.printStackTrace();
+		}		
+	}
+	
+	private void loadInstrument(String instrumentName) throws MidiUnavailableException {
+		Synthesizer synth = MidiSystem.getSynthesizer();
+		Instrument mainInst = null;		
+		for (Instrument inst : synth.getAvailableInstruments()) {
+			String name = inst.getName();
+			if (name.equals(instrumentName)) {
+				System.out.println("Using instrument: " + name);
+				mainInst = inst;
+				break;
+			}
 		}
-		
-//		Synthesizer synth = MidiSystem.getSynthesizer();
-//		Instrument mainInst = null;		
-//		for (Instrument inst : synth.getAvailableInstruments()) {
-//			String name = inst.getName();
-//			if (name.equals(instrumentName)) {
-//				System.out.println("Using instrument: " + name);
-//				mainInst = inst;
-//				break;
-//			}
-//		}
-//		synth.loadInstrument(mainInst);		
+		synth.loadInstrument(mainInst);
 	}
 }
